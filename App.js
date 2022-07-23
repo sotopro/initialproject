e4e03ffd1ce6c945d137667461025658ec7e2eee
sotopro <dasg.1993@gmail.com>
2022-07-23 12:46:37 -0500

    [feature] - add state to item & item liswt & add validation for button and input

--- App.js
+++ App.js
@@ -1,14 +1,48 @@
-import { StyleSheet, Text, View, Button, TextInput, SafeAreaView } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [item, setItem] = useState('');
+  const [itemList, setItemList] = useState([]);
+
+  const inputvalue = item.trim();
+
+
+  const onChangeText = (text) => {
+    setItem(text);
+  }
+
+  const addItem = () => {
+      if (inputvalue) {
+        setItemList([
+          ...itemList,
+          {
+            id: Math.random().toString(),
+            value: item
+          } 
+        ])
+        setItem('');
+      }
+  }
+
   return (
     <View style={styles.container}>
      <View style={styles.inputContainer}>
-      <TextInput style={styles.input} placeholderTextColor='#cccccc' placeholder='add item'/>
-      <Button title='Add' color='#7D8CC4' />
+      <TextInput 
+        style={styles.input}
+        placeholderTextColor='#cccccc'
+        placeholder='add item'
+        value={item}
+        onChangeText={onChangeText}
+      />
+      <Button title='Add' color='#7D8CC4' onPress={addItem} disabled={inputvalue === ''}/>
      </View>
-     <View>
-      <Text>Lista</Text>
+     <View style={styles.itemList}>
+      {itemList.map((item) => (
+        <View key={`item-${item.id}`} style={styles.itemContainer}>
+          <Text style={styles.item}>{item.value}</Text>
+        </View>
+      ))}
      </View>
     </View>
   );
@@ -30,5 +64,8 @@ const styles = StyleSheet.create({
     width: '80%',
     borderBottomWidth: 1,
     borderBottomColor: '#7D8CC4',
-  }
+  },
+  itemList: {},
+  itemContainer: {},
+  item: {}
 });
